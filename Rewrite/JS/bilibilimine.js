d0db4e3c65618505992d115f81e5543bcbde2b67
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-11 02:54:49 +0800

    Update BiliBilimine.js

--- Rewrite/JS/bilibilimine.js
+++ Rewrite/JS/bilibilimine.js
@@ -1,3 +1,4 @@
+//修改内容
 let new_items = [{
                         "id": 501,
                         "title": "设置",
@@ -6,6 +7,34 @@ let new_items = [{
                         "common_op_item": {}
                     }];
 
-let response = $response.body;
-response = JSON.parse(response);
-response
+//获取原始响应体
+let new_response_body = $response.body;
+//判断响应体是否为空，若不为空：
+if(new_response_body){
+  try{
+  //将响应体转化为对象
+  new_response_body = JSON.parse(new_response_body);
+  //判断路径是否存在，不存在则创建并赋值为一个空数组
+  if(!new_response_body.data){
+    new_response_body.data = {};}
+  if(!new_response_body.data.sections_v2){
+    new_response_body.data.sections_v2 = {};}
+  if(!new_response_body.data.sections_v2.items){
+    new_response_body.data.sections_v2.items = {};}
+  
+    //修改原始响应体内容
+    new_response_body.data.sections_v2.items = new_items;
+    //转化为json
+    new_response_body = JSON.stringify(new_response_body);
+    $done({ body: new_response_body});    
+  }catch (e) {
+    //如果响应体不是JSON格式，直接返回原始响应体
+    console.log("响应体不是json格式:", e);
+    $done({ body: new_response_body});
+}
+//若为空
+} else {
+  // 如果没有响应体，直接返回
+  console.log("错误：无响应体");
+  $done();
+}
