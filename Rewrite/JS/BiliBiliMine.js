64bd42e21275b8e030934ad645098a5a7c1f490d
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-11 03:46:07 +0800

    Update BiliBiliMine.js

--- Rewrite/JS/BiliBiliMine.js
+++ Rewrite/JS/BiliBiliMine.js
@@ -1,5 +1,5 @@
 
-console.log("0.1")
+console.log("0.1");
 let new_items = [{
                         "id": 501,
                         "title": "设置",
@@ -10,7 +10,7 @@ let new_items = [{
 
 
 let new_response_body = $response.body;
-console.log("0.2")
+console.log("0.2");
 
 if(new_response_body){
   try{
@@ -31,7 +31,7 @@ if(new_response_body){
     new_response_body.data.sections_v2.items = new_items;
 
     new_response_body = JSON.stringify(new_response_body);
-    console.log("0.3")
+    console.log("0.3");
     $done({ body: new_response_body});    
   }catch (e) {
 
