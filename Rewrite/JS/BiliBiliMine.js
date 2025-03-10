399c6b78374bc2225e97cd416185a2109125a745
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-11 03:52:02 +0800

    Update BiliBiliMine.js

--- Rewrite/JS/BiliBiliMine.js
+++ Rewrite/JS/BiliBiliMine.js
@@ -1,46 +1,45 @@
-
 console.log("0.1");
 let new_items = [{
-                        "id": 501,
-                        "title": "设置",
-                        "uri": "bilibili://user_center/setting",
-                        "icon": "http://i0.hdslb.com/bfs/archive/e932404f2ee62e075a772920019e9fbdb4b5656a.png",
-                        "common_op_item": {}
-                    }];
-
+                    "id": 501,
+                    "title": "设置",
+                    "uri": "bilibili://user_center/setting",
+                    "icon": "http://i0.hdslb.com/bfs/archive/e932404f2ee62e075a772920019e9fbdb4b5656a.png",
+                    "common_op_item": {}
+                }];
 
 let new_response_body = $response.body;
 console.log("0.2");
 
 if(new_response_body){
   try{
+    new_response_body = JSON.parse(new_response_body);
 
-  new_response_body = JSON.parse(new_response_body);
+    if(!new_response_body.data){
+      new_response_body.data = {};
+      console.log("1");
+    }
+    if(!new_response_body.data.sections_v2){
+      new_response_body.data.sections_v2 = {};
+      console.log("2");
+    }
+    if(!Array.isArray(new_response_body.data.sections_v2.items)){
+      new_response_body.data.sections_v2.items = [];
+      console.log("3");
+    }
 
-  if(!new_response_body.data){
-    new_response_body.data = {};
-    console.log("1");}
-  if(!new_response_body.data.sections_v2){
-    new_response_body.data.sections_v2 = {};
-    console.log("2";}
-  if(!new_response_body.data.sections_v2.items){
-    new_response_body.data.sections_v2.items = {};
-    console.log("3")}
-  
-
-    new_response_body.data.sections_v2.items = new_items;
+    new_response_body.data.sections_v2.items.push(...new_items);
 
     new_response_body = JSON.stringify(new_response_body);
     console.log("0.3");
-    $done({ body: new_response_body});    
-  }catch (e) {
-
+    $done({ body: new_response_body });    
+  } catch (e) {
     console.log("响应体不是json格式:", e);
-    $done({ body: new_response_body});
-}
-
+    $done({ body: new_response_body });
+  }
 } else {
-
   console.log("错误：无响应体");
-  $done();
+  $done({});
 }
+
+
+
