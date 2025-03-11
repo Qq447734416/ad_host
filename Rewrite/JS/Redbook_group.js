da6573fa53238227f9e9000b8a377cbebbd171f5
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-11 23:03:35 +0800

    Create Redbook_group.js

---
+++ Rewrite/JS/Redbook_group.js
@@ -0,0 +1,27 @@
+//删除小红书消息页面群聊文字
+
+
+body = $response.body;
+//如果响应体存在
+if(body){
+  //转换格式
+  try{
+    body = JSON.parse(body);
+    //赋值为一个空字符串
+    body.data.plus_sign.entrance.desc = "";
+    body.data.plus_sign.entrance.descCn = "";
+    //转换格式
+    body = JSON.stringify(body);
+
+    $done({body:body);
+  }catch(e){
+    console.error("修改响应体失败：", e);
+    $done()
+  }
+  
+  
+}else{
+  //响应体为空
+  console.error("响应体内容为空");
+  $done();
+}
