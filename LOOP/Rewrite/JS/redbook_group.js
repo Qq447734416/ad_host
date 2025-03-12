5d5abccbf5f5454ff13c3e9b6efe476587856b19
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-12 23:49:21 +0800

    Update redbook_group.js

--- LOOP/Rewrite/JS/redbook_group.js
+++ LOOP/Rewrite/JS/redbook_group.js
@@ -4,18 +4,20 @@
 body = $response.body;
 //如果响应体存在
 if(body){
-  //转换格式
   try{
+    //转换格式
     body = JSON.parse(body);
     //赋值为一个空字符串
     body.data.plus_sign.entrance.desc = "";
     body.data.plus_sign.entrance.descCn = "";
     //转换格式
     body = JSON.stringify(body);
-
     $done({body:body});
   }catch(e){
     console.log("修改响应体失败：", e);
+    let title = "";
+    let subtitle = "";
+    let content = "";
     $done({});
   }
   
