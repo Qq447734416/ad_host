5c2e8ca5153d0c26a5e5c57acbd84e7ab09c0d8e
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-13 05:57:52 +0800

    Create 1.js

---
+++ LOOP/Rewrite/JS/1.js
@@ -0,0 +1,42 @@
+//关闭QQ音乐一些debug
+ "debugIdReport": true,
+      "globalSwitch": true,
+      "safeUniqueIdReport": true,
+      "samplingEventReport": true,
+      "uniqueIdReport": true,
+      "vendorOAIDReport": true
+
+let body = $response.body;
+//请求体不为空则尝试修改，请求体为空或修改失败返回原始数据
+if(body){
+  try{
+    body = JSON.parse(body);
+    //修改数据
+    body.data.switch.debugIdReport = false;
+    body.data.switch.globalSwitch = false;
+    body.data.switch.safeUniqueIdReport = false;
+    body.data.switch.samplingEventReport = false;
+    body.data.switch.uniqueIdReport = false;
+    body.data.switch.vendorOAIDReport = false;
+    //返回修改后的数据
+    body = JSON.stringify(body);
+    console.log("修改成功");
+    $done({body:body});
+  }catch(e){
+    console.log("修改响应体失败：", e);
+    //发送通知
+    let title = "脚本运行错误";
+    let content = "修改响应体失败,脚本：" + $script.name;
+    $notification.post({title, subtitle:"", content });
+    //返回原始数据
+    $done({});
+  }
+}else{
+    console.log("错误，响应体为空");
+    //发送通知
+    let title = "脚本运行错误";
+    let content = "错误，响应体为空,脚本：" + $script.name;
+    $notification.post({title, subtitle:"", content });
+    //返回原始数据
+    $done({});
+}
