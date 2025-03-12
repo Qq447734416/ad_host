bb3e092102c90875cf5c354e92674d9d87b65414
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-12 21:05:41 +0800

    Update redbook_tab.js

--- LOOP/Rewrite/JS/redbook_tab.js
+++ LOOP/Rewrite/JS/redbook_tab.js
@@ -1,1 +1,39 @@
+//小红书侧边栏精简
+
+
+let new_array = [
+        "sidebar_config_draft",
+        "sidebar_config_home_comment",
+        "sidebar_config_history",
+        "sidebar_config_divider",
+        "sidebar_config_order",
+        "sidebar_config_cart",
+        "sidebar_config_wallet",
+        "sidebar_config_divider",
+        "sidebar_config_applets_homepage",
+    ];
+
+//获取原始响应体
+let body = $request.body;
+
+//如果body存在存在，则修改并返回修改后的内容，如果不存在则返回原始内容
+if(body){
+  //尝试将body转化为JS对象并修改数据,失败则返回原始内容
+  try{
+    body = JSON.parse(body);
+    //修改数据
+    body.data.sideConfigHomepage.hierarchy.structure.body = new_array;
+    body.data.sideConfigPersonalPage.hierarchy.structure.body = new_array;
+    body = JSON.stringify(body);
+    $done({body:body});
+    console.log("修改成功。");    
+  }catch(e){
+    console.error("错误：",e );
+    $done({});  
+  }
+}else{
+  console.error("错误，响应体内容为空");
+  $done({});
+}
+
 
