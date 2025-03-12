37de235e47bd5ab9d9427f150e22c16a8968dbb1
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-13 02:50:20 +0800

    Create bilibili_mine.js

---
+++ LOOP/Rewrite/JS/bilibili_mine.js
@@ -0,0 +1,43 @@
+//精简国际版B站“我的”页面tab
+
+
+let body = $response.body;
+//若请求体不为空
+if(body){
+  try{
+    //修改数据
+    body = JSON.parse(body);
+    let setting_index = body.data.sections_v2[1].items.findIndex(item => item.title === '设置');  //找到设置的索引
+    //确保已正确取到索引，若未取到索引则返回原始数据
+    if(setting_index !== -1){
+      let new_items = [body.data.sections_v2[1].items[setting_index]];
+      body.data.sections_v2[1].items = new_items;
+      //返回修改后的数据
+      body = JSON.stringify(body);
+      $done({body:body});
+    }else{
+      console.log("未找到”设置“的索引");
+      //发送通知
+      let title = "脚本运行错误";
+      let content = "未找到”设置“的索引，脚本：" + $script.name;
+      $notification({title, subtitle:"", content});
+      //返回原始数据
+      $done({});
+    } 
+  }catch(e){
+    console.log("修改请求体失败",e );
+    //发送通知
+    let title = "脚本运行失败";
+    let content = "修改请求体失败,脚本：" + $script.name;
+    $notification({title, subtitle:"", content});
+    //返回原始数据
+    $done({});
+  }
+}else{
+  console.log("响应体为空");
+  let title = "脚本运行错误";
+  let content = "响应体为空，脚本：" + $script.name;
+  $notification({title, subtitle:"", content});
+  //返回原始数据
+  $done({});
+}
