ae9a41eb98a9b57c5d740dd61f73a3e19a438a8c
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-16 11:27:23 +0800

    Update update_GitHubHost.py

--- scripts/update_GitHubHost.py
+++ scripts/update_GitHubHost.py
@@ -8,7 +8,7 @@ except requests.RequestException as e:
     print(f"HTTP请求失败: {e}")
     exit(1)
 
-path = r"LOOP/plugin/rewrite.plugin"
+path = r"LOOP/plugin/GitHub.plugin"
 with open(path, "w", encoding="utf-8") as file:
     file.write("//通过DNS映射解决无法访问GitHub问题\n")
     file.write(f"//更新时间：{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n")
