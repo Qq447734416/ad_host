1cb65d1c687eec2040a5e458229612f0254adcc0
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-16 10:54:54 +0800

    Update update_GitHubHost.py

--- scripts/update_GitHubHost.py
+++ scripts/update_GitHubHost.py
@@ -11,6 +11,6 @@ except requests.RequestException as e:
 path = r"LOOP/plugin/rewrite.plugin"
 with open(path, "w", encoding="utf-8") as file:
     file.write("//通过DNS映射解决无法访问GitHub问题\n")
-    file.write(f"//更新时间：{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+    file.write(f"//更新时间：{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n")
     for x in host_ip:
         file.write(f"{x[1]} = {x[0]}\n")
