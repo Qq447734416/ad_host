8dc4b5b6de0c9c4f4820e09706e9b981427c4069
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-17 23:44:40 +0800

    Update blackbox_mine.js

--- LOOP/Rewrite/JS/blackbox_mine.js
+++ LOOP/Rewrite/JS/blackbox_mine.js
@@ -1,4 +1,31 @@
 //删除"我的"页面游戏小卡片
 //删除"我的任务"下方小字
 
-let body = $response。body
+let body = $response.body;
+if(body){
+  try{
+    body = JSON.parse(body);
+    //修改数据
+    body.result.result.game_card_setting = {};
+    body.result.game_cards = [];
+    //返回数据
+    body = JSON.stringify(body);
+    $done({body:body});
+  }catch(e){
+    console.log("修改请求体失败", e);
+    //发送一个本地通知
+    let title = "脚本运行错误";
+    let content = "修改请求体失败，脚本：" + $script.name;
+    $notification.post({title, subtitle:"", content });
+    //返回原始数据
+    $done({});
+  }
+}else{
+  console.log("错误，响应体为空");
+  //发送本地通知
+  let title = "脚本运行错误";
+  let content = "响应体为空，脚本：" + $script.name;
+  $notification.post({title, subtitle:"", content});
+  //返回原始数据
+  $done({});
+}
