4a061fd87958585713dbcd624d792c2cfea0a709
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-18 03:09:46 +0800

    Update redbook_homefeed_ad.js

--- LOOP/Rewrite/JS/redbook_homefeed_ad.js
+++ LOOP/Rewrite/JS/redbook_homefeed_ad.js
@@ -3,6 +3,7 @@
 
 let body = $response.body;
 if(body){
+  console.log(1);
   try{
     body = JSON.parse(body);
     body.data = body.data.filter(item => item.is_ads === false);
@@ -12,16 +13,18 @@ if(body){
     console.log("修改请求体失败",e );
     //发送通知
     let title = "脚本运行失败";
+    let subtitle = "";
     let content = "修改请求体失败,脚本：" + $script.name;
-    $notification.post({title,content});
+    $notification.post({title,subtitle, content});
     //返回原始数据
     $done({});
   }
 }else{
   console.log("响应体为空");
   let title = "脚本运行错误";
+  let subtitle = "";
   let content = "响应体为空，脚本：" + $script.name;
-  $notification.post({title,content});
+  $notification.post({title,subtitle, content});
   //返回原始数据
   $done({});
 }
