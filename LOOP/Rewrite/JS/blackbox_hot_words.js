218dc878e8076ce27fb448337ce33708173d9b9b
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-17 22:36:16 +0800

    Create blackbox_hot_words.js

---
+++ LOOP/Rewrite/JS/blackbox_hot_words.js
@@ -0,0 +1,22 @@
+//去除小黑盒热搜内容
+
+
+let new_hot_words = {
+    "msg": "",
+    "result": {
+        "mini_programs": [],
+        "ads_banner": [],
+        "list": [],
+        "default_q": "",
+        "header": "",
+        "hashtag_list": []
+    },
+    "status": "ok"
+};
+
+let body = $response.body;
+if(body){
+  try{
+    body
+  }
+}
