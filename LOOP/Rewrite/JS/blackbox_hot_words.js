cd03203278a85c2b0f1be0ec3a5a36643cdb5969
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-17 22:42:43 +0800

    Update blackbox_hot_words.js

--- LOOP/Rewrite/JS/blackbox_hot_words.js
+++ LOOP/Rewrite/JS/blackbox_hot_words.js
@@ -14,9 +14,24 @@ let new_hot_words = {
     "status": "ok"
 };
 
-let body = $response.body;
 if(body){
   try{
-    body
+    $done({body:new_hot_words});
+  }catch(e){
+    console.log("修改请求体失败", e);
+    //发送一个本地通知
+    let title = "脚本运行错误";
+    let content = "修改请求体失败，脚本：" + $script.name;
+    $notification.post({title, subtitle:"", content });
+    //返回原始数据
+    $done({});
   }
+}else{
+  console.log("错误，响应体为空");
+  //发送本地通知
+  let title = "脚本运行错误";
+  let content = "响应体为空，脚本：" + $script.name;
+  $notification.post({title, subtitle:"", content});
+  //返回原始数据
+  $done({});
 }
