b7f0607433ea79e9affc10fe945f0c51708e530f
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-20 18:07:37 +0800

    Update blackbox_delete_feedad.js

--- LOOP/Rewrite/JS/blackbox_delete_feedad.js
+++ LOOP/Rewrite/JS/blackbox_delete_feedad.js
@@ -4,24 +4,27 @@
 let body = $response.body;
 if(body){
   try{
+    //修改数据
     body = JSON.parse(body);
     body.result.ad_info = {};
     body = JSON.stringify(body);
+    //返回数据
+    console.log(`修改成功, 时间:${$script.startTime}`);
     $done({body:body});
   }catch(e){
-    console.log("修改请求体失败",e );
     //发送通知
     let title = "脚本运行失败";
     let content = "修改请求体失败,脚本：" + $script.name;
-    $notification.post({title,"", content});
+    $notification.post(title, "", content);
+    console.log(`修改请求体失败, 时间：${$script.startTime}, 错误：${e}`);
     //返回原始数据
     $done({});
   }
 }else{
-  console.log("响应体为空");
   let title = "脚本运行错误";
   let content = "响应体为空，脚本：" + $script.name;
-  $notification.post({title, "", content});
+  $notification.post(title, "", content);
+  console.log(`响应体为空，时间：${$script.startTime}`);
   //返回原始数据
   $done({});
 }
