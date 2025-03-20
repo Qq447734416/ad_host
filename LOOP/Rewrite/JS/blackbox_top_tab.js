501011811bbf9b44c2563389c70c067280aa33be
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-20 18:17:01 +0800

    Update blackbox_top_tab.js

--- LOOP/Rewrite/JS/blackbox_top_tab.js
+++ LOOP/Rewrite/JS/blackbox_top_tab.js
@@ -10,11 +10,11 @@ if(body){
     body = JSON.stringify(body);
     $done({body:body});
   }catch(e){
-    console.log("修改请求体失败",e );
+    console.log("修改请求体失败" + e );
     //发送通知
     let title = "脚本运行失败";
     let content = "修改请求体失败,脚本：" + $script.name;
-    $notification.post({title, subtitle:"", content});
+    $notification.post(title, "", content);
     //返回原始数据
     $done({});
   }
@@ -22,7 +22,7 @@ if(body){
   console.log("响应体为空");
   let title = "脚本运行错误";
   let content = "响应体为空，脚本：" + $script.name;
-  $notification.post({title, subtitle:"", content});
+  $notification.post(title, "", content);
   //返回原始数据
   $done({});
 }
