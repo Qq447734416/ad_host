10777b6187b931f84e0f78991b8debbb1a3e6ac4
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-03-20 19:11:32 +0800

    Update redbook_search_feed_ad.js

--- LOOP/Rewrite/JS/redbook_search_feed_ad.js
+++ LOOP/Rewrite/JS/redbook_search_feed_ad.js
@@ -2,4 +2,28 @@
 
 
 let body = $response.body;
-body = JSON.parse(body);
+//响应体存在
+if(body){
+  try{
+  body = JSON.parse(body);
+  body.data.items = body.data.items.filter(item => item.model_type === 'note');
+  //若修改成功,返回修改后的数据
+  if(body.data.items){
+    body = JSON.stringify(body);
+    console.log("修改成功");
+    $done({body:body});
+  }else{  //返回原始数据
+    console.log("错误，修改后的数组为空");
+    $notification.post("脚本运行错误", "", $script.name);
+    $done({});
+  }
+}catch(e){  //返回原始数据
+    console.log("修改数据时出现错误：" + e);
+    $notification.post("脚本运行错误", "", $script.name);
+    $done({});
+}
+}else{  //返回原始数据
+  console.log("错误，响应体为空");
+  $notification.post("脚本运行错误", "", $script.name);
+  $done({});
+}
