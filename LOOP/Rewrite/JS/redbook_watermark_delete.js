48b7097cc508b30569c5502546535d1c7072073b
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-04-03 22:35:43 +0800

    Update redbook_watermark_delete.js

--- LOOP/Rewrite/JS/redbook_watermark_delete.js
+++ LOOP/Rewrite/JS/redbook_watermark_delete.js
@@ -1,13 +1,42 @@
 //去除小红书笔记水印
 //强制允许保存
 
-body = $response.body;
+let body = $response.body;
 if(body){
   //尝试修改
   try{
     //转换格式
     body = JSON.parse(body);
-    //修改
+    //判断是否为live图类型笔记,若是则将liveur持久化存储，以配合对live图的去水印
+    if(body.data[0].note_list[0].images_list[0].hasOwnProperty('live_photo')){
+        console.log('存在live图，尝试保存其url');
+        let live_index = 0;
+        let live_url = null;
+        for(const item of body.data[0].note_list[0].images_list){
+            //判断数据存储在那个数组，提取后存储，优先选择h265
+            if(item.live_photo.media.stream.h265.length !== 0){
+                live_url = item.live_photo.media.stream.h265[0].master_url;
+            }else if(item.live_photo.media.stream.h266.length !== 0){
+                live_url = item.live_photo.media.stream.h266[0].master_url;
+            }else if(item.live_photo.media.stream.h264.length !== 0){
+                live_url = item.live_photo.media.stream.h264[0].master_url;
+            }else if(item.live_photo.media.stream.av1.length !== 0){
+                live_url = item.live_photo.media.stream.av1[0].master_url;
+            }else{
+                console.log("错误，未找到需要存储的URL"+live_index)
+            }
+            //尝试存储
+            if($persistentStore.write(live_url, `${live_index}`)){
+                console.log("存储liveURL成功"+live_index)
+            }else{
+                console.log("存储liveURL失败"+live_index)
+            }
+            live_index = live_index + 1;
+        }
+    }else{
+        console.log("此笔记无live图，存储步骤已跳过")
+    }
+    //修改数据
     body.data[0].note_list[0].media_save_config.disable_save = false;
     body.data[0].note_list[0].media_save_config.disable_watermark = true;
     body.data[0].note_list[0].media_save_config.disable_weibo_cover = true;
