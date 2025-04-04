a801fc3a8951da10f5c48730ce874948d081b72d
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-04-04 14:45:33 +0800

    Update bilibili_vedio_ad.js

--- LOOP/Rewrite/JS/bilibili_vedio_ad.js
+++ LOOP/Rewrite/JS/bilibili_vedio_ad.js
@@ -1,10 +1,12 @@
 //删除视频流推广
 
+
+
 let body = $response.body;
 if(body){
     try{
         body = JSON.parse(body);
-        for(const item of body.data.items)
+        for(const item of body.data.items){
             if(item.hasOwnProperty('scroll_guide')){
                 delete item.scroll_guide;
                 console.log("咪咪删除了scroll_guide");
@@ -16,7 +18,7 @@ if(body){
                 console.log("咪咪删除了creative_entrance");
             }else{
                 console.log("牛牛未找到creative_entrance");
-            }
+            }}
         $done({body:JSON.stringify(body)});
     }catch(e){
         console.log("修改失败");
