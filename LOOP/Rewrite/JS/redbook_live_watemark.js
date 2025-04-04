b28a871fb5ad4c2e77dabe9d4f5c043e5048ad47
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-04-04 10:21:52 +0800

    Update redbook_live_watemark.js

--- LOOP/Rewrite/JS/redbook_live_watemark.js
+++ LOOP/Rewrite/JS/redbook_live_watemark.js
@@ -5,14 +5,15 @@ if(body){
     try{
         body = JSON.parse(body);
         let live_index = 0;
-        for(const item of body.data.datas)
+        for(const item of body.data.datas){
             try{
                 item.url = $persistentStore.read(`${live_index}`)
+                console.log("替换URL成功，索引：" + live_index)
             }catch(e){
-                console.log("读取url失败" + live_index);
+                console.log("替换url失败，索引：" + live_index);
                 
             }
-            live_index = live_index + 1;
+            live_index = live_index + 1;}
         $done({body:JSON.stringify(body)});
     }catch(e){
         console.log("修改响应体失败：" + e);
