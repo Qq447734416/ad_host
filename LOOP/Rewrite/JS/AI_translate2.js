102d3c5dcf45ef6e743edf2d8bc64f695406f8c1
momomimi1234 <104320024+Qq447734416@users.noreply.github.com>
2025-04-06 02:51:48 +0800

    Update AI_translate2.js

--- LOOP/Rewrite/JS/AI_translate2.js
+++ LOOP/Rewrite/JS/AI_translate2.js
@@ -2,7 +2,7 @@
 
 let body = JSON.parse($response.body);
 let str = body.choices[0].message.content
-let cleanedStr = str.replace(/[{}\n]/g, '');
+let cleanedStr = str.replace(/[{}\n\\]/g， '');
 body = {
     "翻译结果": cleanedStr
 };
